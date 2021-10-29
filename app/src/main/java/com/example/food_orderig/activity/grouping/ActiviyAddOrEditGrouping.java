5af4ce7900520120a7824316613be5548fbf37dc
nahid.ab <n.alibagheri1998@gmail.com>
2021-10-30 01:56:36 +0330

    fix add or edit grouping

---
+++ app/src/main/java/com/example/food_orderig/activity/grouping/ActiviyAddOrEditGrouping.java
@@ -0,0 +1,16 @@
+package com.example.food_orderig.activity.grouping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.food_orderig.R;
+
+public class ActiviyAddOrEditGrouping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiy_add_or_edit_grouping);
+    }
+}
